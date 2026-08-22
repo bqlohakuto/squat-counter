f7c5fd4c257ac95e15760c595842d1e1baa34b2c
bqlohakuto <nzkj6d7pj8@privaterelay.appleid.com>
2026-08-23 04:43:12 +0900

    Use supplied staff artwork at the counter

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE = "squat-bar-v8";
-const ASSETS = ["./", "./index.html", "./style.css", "./app.js", "./manifest.webmanifest", "./icon.svg", "./maid-character-no-halo.png"];
+const CACHE = "squat-bar-v9";
+const ASSETS = ["./", "./index.html", "./style.css", "./app.js", "./manifest.webmanifest", "./icon.svg", "./a_clean_cutout_transparent_background_illustration.png"];
 self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener("activate", event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
 self.addEventListener("fetch", event => event.respondWith(caches.match(event.request).then(response => response || fetch(event.request))));
