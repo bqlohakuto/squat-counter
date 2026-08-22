eacc007332dbd04f9e53b6ce6299064440107358
bqlohakuto <nzkj6d7pj8@privaterelay.appleid.com>
2026-08-23 04:34:54 +0900

    Show staff behind the bar counter

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE = "squat-bar-v7";
-const ASSETS = ["./", "./index.html", "./style.css", "./app.js", "./manifest.webmanifest", "./icon.svg", "./maid-character-transparent.png"];
+const CACHE = "squat-bar-v8";
+const ASSETS = ["./", "./index.html", "./style.css", "./app.js", "./manifest.webmanifest", "./icon.svg", "./maid-character-no-halo.png"];
 self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener("activate", event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
 self.addEventListener("fetch", event => event.respondWith(caches.match(event.request).then(response => response || fetch(event.request))));
